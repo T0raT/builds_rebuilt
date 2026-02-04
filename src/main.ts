5f8f9b88f10c9acf2a8f49ee864f3a69b6e7589a
T0raT <tigerliu@bu.edu>
2026-02-04 12:55:22 -0500

    feat: built hero section

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,45 @@ import "./style.css";
 import fl from "figlet";
 import AnsiShadow from "figlet/fonts/ANSI Shadow";
 
+/**
+ * TODO: Recreate the front page
+ * TODO: SPA functionality (probably hell)
+ * TODO: Nav element + Animations
+ * TODO: Logo animation?
+ * TODO: Think about disabling transformations on mobile and safari...?
+ * */
+
+// Setting up figlet
 fl.parseFont("AnsiShadow", AnsiShadow); // load font
 
 const BU = fl.textSync("BU", { font: "AnsiShadow" }).trim();
 const ILDS = fl.textSync("ILDS", { font: "AnsiShadow" }).trim();
 
+/* HTML SECTION */
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-<div id="logo-container">
+<header id="logo-container">
   <pre style='color:red;'>${BU}</pre>
   <pre>${ILDS}</pre>
-</div>
-<section class="inner">
-</section>
+</header>
+<main class="inner">
+  <section class="hero-section">
+    <h1><span>BU</span> INFORMATION LAB AND DESIGN SPACE</h1>
+    <p>BUILDS is an inclusive group of students, artists, hackers, and organizers who believe in the do-it-yourself attitude and hacker ethic.
+    </p>
+    <div class='loc'>@ 665 Commonwealth Ave | Room 220</div>
+    <div class='btn-container'>
+      <Button>EVENTS</Button>
+      <Button>JOIN MAILING LIST</Button>
+    </div>
+  </section>
+  <section class="builds-activities">
+    <h2>WHAT CAN YOU DO AT BUILDS?</h2>
+  </section>
+</main>
 `;
 
+// Grabbing necessary HTML elements;
 const appDiv = document.querySelector<HTMLDivElement>("#app");
 const appContainerDiv =
   document.querySelector<HTMLDivElement>("#app-container");
@@ -23,25 +48,28 @@ const innerDiv = document.querySelector<HTMLDivElement>("#app > .inner");
 if (!appDiv || !innerDiv || !appContainerDiv)
   throw new Error("Critical elements missing from DOM");
 
+// Single function that handles page element transformations
 const transformDiv = (e: MouseEvent) => {
   let { top, height, left, width } = appDiv.getBoundingClientRect();
   const mouseX = (e.clientX - left) / width;
   const mouseY = (e.clientY - top) / height;
-  const rotateY = (0.5 - mouseX) * 10;
-  const rotateX = (mouseY - 0.5) * 10;
+  const rotateY = (0.5 - mouseX) * 18;
+  const rotateX = (mouseY - 0.5) * 18;
   const shiftX = (0.5 - mouseX) * 10;
   const shiftY = (0.5 - mouseY) * 10;
 
-  appDiv.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(${(0.5 - mouseY) * 10}px) translateX(${(0.5 - mouseX) * 10}px)`;
+  //appDiv.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(${(0.5 - mouseY) * 10}px) translateX(${(0.5 - mouseX) * 10}px)`;
 
-  innerDiv.style.transform = `translateY(${shiftY}px) translateX(${shiftX}px) translateZ(10px)`;
-  appContainerDiv.style.transform = `translateY(${shiftY}px) translateX(${shiftX}px)`;
+  innerDiv.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(${shiftY}px) translateX(${shiftX}px) translateZ(10px)`;
+
+  //appContainerDiv.style.transform = `translateY(${shiftY}px) translateX(${shiftX}px)`;
 
   // Debug Data
-  console.log(`rotateY:${rotateY}\nrotateX:${rotateX}`);
+  //console.log(`rotateY:${rotateY}\nrotateX:${rotateX}`);
   //console.log(`x:${mouseX} \nY:${mouseY}`);
 };
 
+// To reset transformations when user cursors leave main content
 const resetDiv = () => {
   appDiv.style.transform = "rotateX(0deg) rotateY(0deg)";
   innerDiv.style.transform = `translateY(0px) translateX(0px)`;
@@ -49,6 +77,6 @@ const resetDiv = () => {
 };
 
 appContainerDiv.addEventListener("mousemove", transformDiv);
-appContainerDiv.addEventListener("mouseenter", transformDiv);
+//appContainerDiv.addEventListener("mouseenter", transformDiv);
 appContainerDiv.addEventListener("mouseleave", resetDiv);
 addEventListener("resize", resetDiv);
