38c3b136f3bd8736724c5da30c5f7ebb18637ddb
T0raT <tigerliu@bu.edu>
2026-02-04 09:33:50 -0500

    feat: added figlet builds logo

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,26 @@
 import "./style.css";
+import fl from "figlet";
+import AnsiShadow from "figlet/fonts/ANSI Shadow";
+
+fl.parseFont("AnsiShadow", AnsiShadow); // load font
+
+const BU = fl.textSync("BU", { font: "AnsiShadow" }).trim();
+const ILDS = fl.textSync("ILDS", { font: "AnsiShadow" }).trim();
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-<pre id='logo'>
-</pre>
-    <section class="inner">
-    </section>
+<div id="logo-container">
+  <pre style='color:red;'>${BU}</pre>
+  <pre>${ILDS}</pre>
+</div>
+<section class="inner">
+</section>
 `;
 
 const appDiv = document.querySelector<HTMLDivElement>("#app");
 const appContainerDiv =
   document.querySelector<HTMLDivElement>("#app-container");
 const innerDiv = document.querySelector<HTMLDivElement>("#app > .inner");
-const logoPre = document.querySelector<HTMLPreElement>("#logo");
-if (!appDiv || !innerDiv || !appContainerDiv || !logoPre)
+if (!appDiv || !innerDiv || !appContainerDiv)
   throw new Error("Critical elements missing from DOM");
 
 const transformDiv = (e: MouseEvent) => {
