556e2d64a0f0fe0c959444b044c98d803fd69990
T0raT <tigerliu@bu.edu>
2026-04-04 15:42:14 -0400

    fix: lowk astro hates 0x had to switch to ZxProto, same font tho

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,24 +6,24 @@ export default defineConfig({
   fonts: [
     {
       provider: fontProviders.local(),
-      name: "0xProto",
-      cssVariable: "--font-0xProto",
+      name: "ZxProto",
+      cssVariable: "--font-ZxProto",
       options: {
         variants: [
           {
-            src: ["./src/assets/fonts/0xProto-Regular.woff2"],
+            src: ["./src/assets/fonts/ZxProto-Regular.ttf"],
             weight: "normal",
             style: "normal",
           },
           {
-            src: ["./src/assets/fonts/0xProto-italic.woff2"],
-            weight: "normal",
-            style: "italic",
-          },
-          {
-            src: ["./src/assets/fonts/0xProto-bold.woff2"],
+            src: ["./src/assets/fonts/ZxProto-Bold.ttf"],
             weight: 700,
             style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/ZxProto-Italic.ttf"],
+            weight: "normal",
+            style: "italic",
           },
         ],
       },
