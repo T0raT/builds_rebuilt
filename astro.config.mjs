b3ea7aaf6bf7191d8fc2172b9f25e421db1ef210
T0raT <tigerliu@bu.edu>
2026-04-04 14:24:34 -0400

    chore: import alias paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "0xProto",
+      cssVariable: "--font-0xProto",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/0xProto-Regular.woff2"],
+            weight: "normal",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/0xProto-italic.woff2"],
+            weight: "normal",
+            style: "italic",
+          },
+          {
+            src: ["./src/assets/fonts/0xProto-bold.woff2"],
+            weight: 700,
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
+});
